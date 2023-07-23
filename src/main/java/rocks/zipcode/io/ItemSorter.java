824f2cdfad9f0d91ea0785a0d6096c8b964e2965
Dmytro_Romaniv <128440171+dimaromaniv@users.noreply.github.com>
2023-07-23 17:17:14 -0400

    I established familiarity with Comparator

--- src/main/java/rocks/zipcode/io/ItemSorter.java
+++ src/main/java/rocks/zipcode/io/ItemSorter.java
@@ -1,15 +1,25 @@
 package rocks.zipcode.io;
 
+import java.util.Arrays;
 import java.util.Comparator;
 
 /**
  * @author leon on 30/01/2019.
  */
 public class ItemSorter {
+
+    private Item[] items;
     public ItemSorter(Item[] items) {
+        this.items = items;
     }
 
     public Item[] sort(Comparator<Item> comparator) {
-        return null;
+
+        Item[] toSort = Arrays.copyOf(items,items.length);
+
+        Arrays.sort(toSort,comparator);
+
+
+        return toSort;
     }
 }
